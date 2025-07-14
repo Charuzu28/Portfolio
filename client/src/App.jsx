7dc393b6bf4d04e16e5404b3620c014506af6388
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-07-14 13:16:00 +0800

    Added project card and its UI and modify some files to make it responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -9,6 +10,9 @@ const App = () => {
     <NavBar/>
     <Routes>
       <Route path="/" element={<Home/>} />
+
+      {/* Fallback */}
+      <Route path="*" element={<NotFound/>} />
     </Routes>
     <Footer />
    </Router>
--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const ProjectCard = (title, image, tech, github, demo) => {
+const ProjectCard = ({title, image, description, link, tags}) => {
   return (
-    <div className='flex'>
-        <img src="" alt="" />
+    <div className='flex flex-col bg-gray-50 border border-gray-300 w-full sm:max-w-[90%] lg:max-w-[70%] md:max-w-[40%] rounded-lg shadow-md'>
+        <img 
+        src={image}
+        alt={title}
+        className='rounded-lg' />
+
+        <div className='p-2'>
+          <h3 className='font-semibold px-1'>{title}</h3>
+            <p className='py-1 px-1'>{description}</p>
+            <div className=''>
+              {tags && tags.map((tag, idx) => (
+                <span
+                key={idx}
+                className='px-1'>
+                  {tag}
+                </span>
+              ))}
+            </div>
+
+            <a 
+            href={link}
+            className='py-2 px-1'
+            target='_blank'
+            rel='noopener noreferer'>
+              View project →
+            </a>
+        </div>
     </div>
   )
 }
